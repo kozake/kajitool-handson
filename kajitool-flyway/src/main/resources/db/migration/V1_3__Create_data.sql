13b3361a71de248d3c40ca20b6c0ec8275e0803c
kozake <sh.kozake@gmail.com>
2019-01-24 23:09:31 +0900

    create data

--- kajitool-flyway/src/main/resources/db/migration/V1_3__Create_data.sql
+++ kajitool-flyway/src/main/resources/db/migration/V1_3__Create_data.sql
@@ -1,3 +1,14 @@
 insert into MATERIAL values(1, 'どうのこうせき');
 insert into MATERIAL values(2, 'てつのこうせき');
 insert into MATERIAL values(3, 'ぎんのこうせき');
+
+insert into RECIPE values(RECIPE__ID_SEQ.NEXTVAL, 'どうのつるぎ', 1, SYSDATE);
+insert into RECIPE_DETAIL values(RECIPE_DETAIL__ID_SEQ.NEXTVAL, RECIPE__ID_SEQ.CURRVAL, 1, 3);
+
+insert into RECIPE values(RECIPE__ID_SEQ.NEXTVAL, 'てつのつるぎ', 1, SYSDATE);
+insert into RECIPE_DETAIL values(RECIPE_DETAIL__ID_SEQ.NEXTVAL, RECIPE__ID_SEQ.CURRVAL, 1, 2);
+insert into RECIPE_DETAIL values(RECIPE_DETAIL__ID_SEQ.NEXTVAL, RECIPE__ID_SEQ.CURRVAL, 2, 3);
+
+insert into RECIPE values(RECIPE__ID_SEQ.NEXTVAL, 'せいどうのつるぎ', 1, SYSDATE);
+insert into RECIPE_DETAIL values(RECIPE_DETAIL__ID_SEQ.NEXTVAL, RECIPE__ID_SEQ.CURRVAL, 1, 3);
+insert into RECIPE_DETAIL values(RECIPE_DETAIL__ID_SEQ.NEXTVAL, RECIPE__ID_SEQ.CURRVAL, 2, 1);
