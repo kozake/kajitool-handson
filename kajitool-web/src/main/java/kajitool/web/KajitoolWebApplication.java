cff37f66acdc05699a79b68b17372298ac329250
kozake <kozake@dcr.co.jp>
2019-01-24 13:01:23 +0900

    create kajitool-web

---
+++ kajitool-web/src/main/java/kajitool/web/KajitoolWebApplication.java
@@ -0,0 +1,14 @@
+package kajitool.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KajitoolWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KajitoolWebApplication.class, args);
+	}
+
+}
+
