d4675ea3283060e09a93c51d48613695af14659e
kozake <kozake@dcr.co.jp>
2019-01-24 15:16:15 +0900

    github oauth2

--- kajitool-web/src/main/java/kajitool/web/config/WebSecurityConfig.java
+++ kajitool-web/src/main/java/kajitool/web/config/WebSecurityConfig.java
@@ -1,6 +1,7 @@
 package kajitool.web.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -11,7 +12,16 @@ import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
+        // @formatter:off
         http.csrf().csrfTokenRepository(
-                CookieCsrfTokenRepository.withHttpOnlyFalse());
+                CookieCsrfTokenRepository.withHttpOnlyFalse())
+        .and().oauth2Login()
+        .and().authorizeRequests()
+            .mvcMatchers(HttpMethod.POST, "/api/**/*").authenticated()
+            .mvcMatchers(HttpMethod.PUT, "/api/**/*").authenticated()
+            .mvcMatchers(HttpMethod.DELETE, "/api/**/*").authenticated()
+            .anyRequest().permitAll()
+        ;
+        // @formatter:on
     }
 }
