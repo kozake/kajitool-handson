54a2b69d37133a9189da744bcdbddf5fdcab3157
kozake <kozake@dcr.co.jp>
2019-03-12 15:17:42 +0900

    page route

--- kajitool-ui/src/app/app-routing.module.ts
+++ kajitool-ui/src/app/app-routing.module.ts
@@ -4,8 +4,8 @@ import { Routes, RouterModule } from '@angular/router';
 const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
   { path: 'home', loadChildren: './home/home.module#HomePageModule' },
-  { path: 'select-recipe', loadChildren: './pages/select-recipe/select-recipe.module#SelectRecipePageModule' },
-  { path: 'need-material', loadChildren: './pages/need-material/need-material.module#NeedMaterialPageModule' },
+  // { path: 'select-recipe', loadChildren: './pages/select-recipe/select-recipe.module#SelectRecipePageModule' },
+  // { path: 'need-material', loadChildren: './pages/need-material/need-material.module#NeedMaterialPageModule' },
   { path: 'edit-recipe', loadChildren: './pages/edit-recipe/edit-recipe.module#EditRecipePageModule' },
 ];
 
--- kajitool-ui/src/app/home/home.module.ts
+++ kajitool-ui/src/app/home/home.module.ts
@@ -2,21 +2,48 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { IonicModule } from '@ionic/angular';
 import { FormsModule } from '@angular/forms';
-import { RouterModule } from '@angular/router';
+import { Routes, RouterModule } from '@angular/router';
 
 import { HomePage } from './home.page';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: HomePage,
+    children: [
+      {
+        path: 'select-recipe',
+        children: [
+          {
+            path: '',
+            loadChildren: '../pages/select-recipe/select-recipe.module#SelectRecipePageModule'
+          }
+        ]
+      },
+      {
+        path: 'need-material',
+        children: [
+          {
+            path: '',
+            loadChildren: '../pages/need-material/need-material.module#NeedMaterialPageModule'
+          }
+        ]
+      },
+      {
+        path: '',
+        redirectTo: '/home/select-recipe',
+        pathMatch: 'full'
+      }
+    ]
+  }
+];
 
 @NgModule({
   imports: [
     CommonModule,
     FormsModule,
     IonicModule,
-    RouterModule.forChild([
-      {
-        path: '',
-        component: HomePage
-      }
-    ])
+    RouterModule.forChild(routes)
   ],
   declarations: [HomePage]
 })
